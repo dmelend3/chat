c4fdbb85a1b9b45b623d0ae0301a2da8fdcc2a91
dmelend3 <santos_9602@hotmail.com>
2020-04-19 20:38:23 -0500

    Added Persisted State

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import createPersistedState from "vuex-persistedstate"
 
 Vue.use(Vuex);
 
@@ -15,5 +16,6 @@ export default new Vuex.Store({
   },
   actions: {
     setUser: ({ commit }, user) => commit("settingUser", user)
-  }
+  },
+  plugins: [createPersistedState()]
 })
