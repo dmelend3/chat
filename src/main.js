e994dc9943bdc17cf584b404babc632e1165be3f
dmelend3 <santos_9602@hotmail.com>
2020-04-19 20:16:14 -0500

    finished room logic

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import Vue from "vue";
+import { firestorePlugin } from "vuefire";
+
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 import "./plugins/firebase";
 
+
 Vue.config.productionTip = false;
+
+Vue.use(firestorePlugin)
 
 new Vue({
   router,
